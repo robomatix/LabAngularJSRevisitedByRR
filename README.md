c129a06053f96361ad03da7f19b92ba70abf566d
robomatix <robomatix@robomatix.net>
2015-08-05 07:45:16 +0200

    step 2 first application

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ On my way of learning AngularJS, I decided to revisit this tutorial : https://gi
 It gives a right structure for a project in AngularJS and some good practices !
 
 The article that introduce it ( french ) : http://blog.dcube.fr/blog/2014/10/30/dcube-lab-presentation-angularjs-1/
+
+Step 2 : First aplication... Watch out the js console of your browser...
--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,16 @@
-console.log('TEST');
-alert('HELLO TEST');
+(function () {
+    'use strict';
+
+    var app = angular.module('todo.app', []);
+
+    app.config(appConfig);
+    app.run(appRun);
+
+    function appConfig() {
+        console.log('Configuring application...');
+    };
+
+    function appRun() {
+        console.log('Application started...');
+    };
+})();
