bbe58c6700b92d2408bb3b81242b7966d78f4baa
robomatix <robomatix@robomatix.net>
2015-08-05 09:36:12 +0200

    Filters

--- js/app/controllers/todoController.js
+++ js/app/controllers/todoController.js
@@ -3,8 +3,12 @@
 
     var controllers = angular.module('todo.controllers');
 
-    controllers.controller('TodoController', function ($rootScope, $scope) {
+    controllers.controller('TodoController', function ($rootScope, $scope, $filter, todoPriority) {
         $scope.todoList = [];
+        $scope.todoPriority = todoPriority;
+        $scope.priorityFilter = {};
+        $scope.sortOrder = 'id';
+        $scope.getPriorityName = getPriorityName;
 
         getTodos();
 
@@ -12,8 +16,31 @@
             for (var i = 1; i < 11; i++) {
                 $scope.todoList.push({
                     id: i,
-                    title: 'Todo ' + i
+                    title: 'Todo ' + i,
+                    priority: $scope.todoPriority.high
                 });
+            }
+
+            var oldTodo = $filter('filter')($scope.todoList, function (item) {
+                return item.id > 6;
+            });
+            console.log(JSON.stringify(oldTodo));
+        };
+
+        function getPriorityName() {
+            switch ($scope.priorityFilter.priority) {
+                case $scope.todoPriority.low:
+                    return 'Low';
+                    break;
+                case $scope.todoPriority.medium:
+                    return 'Medium';
+                    break;
+                case $scope.todoPriority.high:
+                    return 'High';
+                    break;
+                default:
+                    return 'None';
+                    break;
             }
         };
     });
